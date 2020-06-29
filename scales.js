e475cc6b13b6b78b969e5f1a33c76f548447a0ef
Jon O'Connor <jon.oconnor27@gmail.com>
2020-06-28 22:42:56 -0400

    Started defining strings and scale patterns.

---
+++ scales.js
@@ -0,0 +1,8 @@
+const E_String = ['E', 'F', 'F#', 'G', 'G#', 'A', 'Bb', 'B', 'C', 'C#', 'D', 'Eb', 'E'];
+const A_String = ['A', 'Bb', 'B', 'C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'G#', 'A'];
+const D_String = [];
+const G_String = [];
+const B_String = [];
+
+const E_Minor_Pentatonic = [0,3,5,7,10];
+
